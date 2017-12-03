c1fa877c3a0ddde3e3e8c1ae0fd1d52c8d7af287
Henry Yang <mail@xiaoming.se>
2017-12-03 18:09:34 +0100

    Refactored Code for problem1

--- src/1/problem1.hs
+++ src/1/problem1.hs
@@ -1,18 +1,15 @@
 calcSum :: [Integer] -> Integer
 calcSum [] = 0
-calcSum nlist = calcSum' n1 n2 ns
+calcSum nlist = calcSum' n1 (ns ++ [n1])
   where
-    [n1,n2] = take 2 nlist
-    ns = (drop 2 nlist) ++ [n1]
+    ([n1],ns) = splitAt 1 nlist
 
-calcSum' :: Integer -> Integer -> [Integer] -> Integer
+calcSum' :: Integer -> [Integer] -> Integer
 --- Basecases first
-calcSum' n1 n2 []
-  | n1 == n2          = n1
-  | otherwise         = 0
-calcSum' n1 n2 (n3:ns)
-  | n1 == n2          = n1 + calcSum' n2 n3 ns
-  | otherwise         = 0 + calcSum' n2 n3 ns
+calcSum' n1 [] = 0
+calcSum' n1 (n2:ns)
+  | n1 == n2   = n1 + calcSum' n2 ns
+  | otherwise  = 0 + calcSum' n2 ns
 
 main = do
   inputString <- readFile "./input.txt"
